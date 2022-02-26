b82b6e9e09192941db1204965b47fe5408ef8627
stasjminister <hans.inge.berg@gmail.com>
2022-02-26 09:53:54 +0100

    Oppdater main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,19 @@
-radio.onReceivedNumber(function (receivedNumber) {
-    kitronik_klip_motor.motorOn(kitronik_klip_motor.Motors.Motor1, kitronik_klip_motor.MotorDirection.Forward, 50)
-    kitronik_klip_motor.motorOn(kitronik_klip_motor.Motors.Motor1, kitronik_klip_motor.MotorDirection.Forward, 50)
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "a") {
+        kitronik_klip_motor.motorOn(kitronik_klip_motor.Motors.Motor1, kitronik_klip_motor.MotorDirection.Forward, 50)
+        kitronik_klip_motor.motorOn(kitronik_klip_motor.Motors.Motor2, kitronik_klip_motor.MotorDirection.Forward, 30)
+    }
+    if (receivedString == "b") {
+        kitronik_klip_motor.motorOn(kitronik_klip_motor.Motors.Motor1, kitronik_klip_motor.MotorDirection.Forward, 30)
+        kitronik_klip_motor.motorOn(kitronik_klip_motor.Motors.Motor2, kitronik_klip_motor.MotorDirection.Forward, 50)
+    }
+    if (receivedString == "ab") {
+        kitronik_klip_motor.motorOn(kitronik_klip_motor.Motors.Motor1, kitronik_klip_motor.MotorDirection.Forward, 50)
+        kitronik_klip_motor.motorOn(kitronik_klip_motor.Motors.Motor2, kitronik_klip_motor.MotorDirection.Forward, 50)
+    }
+    if (receivedString == "stopp") {
+        kitronik_klip_motor.motorOff(kitronik_klip_motor.Motors.Motor1)
+        kitronik_klip_motor.motorOff(kitronik_klip_motor.Motors.Motor1)
+    }
 })
 radio.setGroup(1)
